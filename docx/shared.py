ad718a07778c34e16a57508a93d8258c6f0949eb
Steve Canny <scanny@cisco.com>
2013-12-21 22:17:03 -0800

    shr: add lazyproperty decorator

---
+++ docx/shared.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+"""
+Objects shared by docx modules.
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+def lazyproperty(f):
+    """
+    @lazyprop decorator. Decorated method will be called only on first access
+    to calculate a cached property value. After that, the cached value is
+    returned.
+    """
+    cache_attr_name = '_%s' % f.__name__  # like '_foobar' for prop 'foobar'
+    docstring = f.__doc__
+
+    def get_prop_value(obj):
+        try:
+            return getattr(obj, cache_attr_name)
+        except AttributeError:
+            value = f(obj)
+            setattr(obj, cache_attr_name, value)
+            return value
+
+    return property(get_prop_value, doc=docstring)
