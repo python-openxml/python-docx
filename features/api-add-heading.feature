2438baaed0d79621447c52b12f39d3b030312fc4
Steve Canny <scanny@cisco.com>
2014-01-03 02:14:53 -0800

    acpt: add api-add-heading.feature

---
+++ features/api-add-heading.feature
@@ -0,0 +1,30 @@
+Feature: Add a section heading with text
+  In order add a section heading to a document
+  As a programmer using the basic python-docx API
+  I need a method to add a heading with its text in a single step
+
+  @wip
+  Scenario: Add a heading specifying only its text
+    Given a document
+     When I add a heading specifying only its text
+     Then the style of the last paragraph is 'Heading1'
+      And the last paragraph contains the heading text
+
+  @wip
+  Scenario Outline: Add a heading specifying level
+    Given a document
+     When I add a heading specifying level=<heading level>
+     Then the style of the last paragraph is '<paragraph style>'
+
+   Examples: Heading level styles
+     | heading level | paragraph style |
+     |       0       | Title           |
+     |       1       | Heading1        |
+     |       2       | Heading2        |
+     |       3       | Heading3        |
+     |       4       | Heading4        |
+     |       5       | Heading5        |
+     |       6       | Heading6        |
+     |       7       | Heading7        |
+     |       8       | Heading8        |
+     |       9       | Heading9        |
--- features/steps/api.py
+++ features/steps/api.py
@@ -1,13 +1,27 @@
 # encoding: utf-8
 
 """
-Step implementations for top-level features
+Step implementations for basic API features
 """
 
 from behave import then, when
 
 
 # when ====================================================
+
+@when('I add a heading specifying level={level_str}')
+def when_add_heading_specifying_level(context, level_str):
+    level = int(level_str)
+    document = context.document
+    document.add_heading(level=level)
+
+
+@when('I add a heading specifying only its text')
+def when_add_heading_specifying_only_its_text(context):
+    document = context.document
+    context.heading_text = 'Spam vs. Eggs'
+    document.add_heading(context.heading_text)
+
 
 @when('I add a paragraph specifying its style')
 def when_add_paragraph_specifying_style(context):
@@ -31,12 +45,28 @@ def when_add_paragraph_without_specifying_text_or_style(context):
 
 # then =====================================================
 
+@then('the last paragraph contains the heading text')
+def then_last_p_contains_heading_text(context):
+    document = context.document
+    text = context.heading_text
+    p = document.paragraphs[-1]
+    assert p.text == text
+
+
 @then('the last paragraph contains the text I specified')
 def then_last_p_contains_specified_text(context):
     document = context.document
     text = context.paragraph_text
     p = document.paragraphs[-1]
     assert p.text == text
+
+
+@then('the last paragraph has the style I specified')
+def then_last_p_has_specified_style(context):
+    document = context.document
+    style = context.paragraph_style
+    p = document.paragraphs[-1]
+    assert p.style == style
 
 
 @then('the last paragraph is the empty paragraph I added')
@@ -46,9 +76,8 @@ def then_last_p_is_empty_paragraph_added(context):
     assert p.text == ''
 
 
-@then('the last paragraph has the style I specified')
-def then_last_p_has_specified_style(context):
+@then('the style of the last paragraph is \'{style}\'')
+def then_style_of_last_paragraph_is_style(context, style):
     document = context.document
-    style = context.paragraph_style
     p = document.paragraphs[-1]
     assert p.style == style
