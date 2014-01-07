259fcfd900005a800ab4f3d943a27e7ce6829005
Steve Canny <scanny@cisco.com>
2014-01-07 06:17:03 -0800

    oxml: add XmlString for xml __eq__ override
    lxml on Python 3 is not so predictable as to how it orders namespace
    declarations when serializing XML output. Because the unit tests
    generally rely on a string compare to confirm to XML elements are
    equal, this override on the OxmlBaseElement.xml.__eq__() is required to
    avoid false positives when namespace and/or attribute order differs but
    their contents do not.

--- docx/oxml/shared.py
+++ docx/oxml/shared.py
@@ -6,6 +6,8 @@ Objects shared by modules in the docx.oxml subpackage.
 
 from lxml import etree
 
+import re
+
 from .exceptions import ValidationError
 
 
@@ -145,11 +147,77 @@ def serialize_for_reading(element):
     Serialize *element* to human-readable XML suitable for tests. No XML
     declaration.
     """
-    return etree.tostring(element, encoding='unicode', pretty_print=True)
+    xml = etree.tostring(element, encoding='unicode', pretty_print=True)
+    return XmlString(xml)
 
 
 def _SubElement(parent, tag):
     return etree.SubElement(parent, qn(tag), nsmap=nsmap)
+
+
+class XmlString(str):
+    """
+    Provides string comparison override suitable for serialized XML that is
+    useful for tests.
+    """
+
+    # '    <w:xyz xmlns:a="http://ns/decl/a" attr_name="val">text</w:xyz>'
+    # |          |                                          ||           |
+    # +----------+------------------------------------------++-----------+
+    #  front      attrs                                     | text
+    #                                                     close
+
+    _xml_elm_line_patt = re.compile(
+        '( *</?[\w:]+)(.*?)(/?>)([^<]*</\w+>)?'
+    )
+
+    def __eq__(self, other):
+        lines = self.splitlines()
+        lines_other = other.splitlines()
+        if len(lines) != len(lines_other):
+            return False
+        for line, line_other in zip(lines, lines_other):
+            if not self._eq_elm_strs(line, line_other):
+                return False
+        return True
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def _attr_seq(self, attrs):
+        """
+        Return a sequence of attribute strings parsed from *attrs*. Each
+        attribute string is stripped of whitespace on both ends.
+        """
+        attrs = attrs.strip()
+        attr_lst = attrs.split()
+        return sorted(attr_lst)
+
+    def _eq_elm_strs(self, line, line_2):
+        """
+        Return True if the element in *line_2* is XML equivalent to the
+        element in *line*.
+        """
+        front, attrs, close, text = self._parse_line(line)
+        front_2, attrs_2, close_2, text_2 = self._parse_line(line_2)
+        if front != front_2:
+            return False
+        if self._attr_seq(attrs) != self._attr_seq(attrs_2):
+            return False
+        if close != close_2:
+            return False
+        if text != text_2:
+            return False
+        return True
+
+    def _parse_line(self, line):
+        """
+        Return front, attrs, close, text 4-tuple result of parsing XML element
+        string *line*.
+        """
+        match = self._xml_elm_line_patt.match(line)
+        front, attrs, close, text = [match.group(n) for n in range(1, 5)]
+        return front, attrs, close, text
 
 
 # ===========================================================================
