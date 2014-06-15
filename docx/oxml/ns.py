5ec3ac17363b2d34a19c009d790099f97fda7b3c
Steve Canny <scanny@cisco.com>
2014-06-15 02:24:51 -0700

    oxml: extract NamespacePrefixedTag to docx.oxml.ns
    And added unit tests for NamespacePrefixedTag

--- docx/oxml/ns.py
+++ docx/oxml/ns.py
@@ -19,3 +19,54 @@ nsmap = {
             'Drawing'),
     'xml': ('http://www.w3.org/XML/1998/namespace')
 }
+
+
+class NamespacePrefixedTag(str):
+    """
+    Value object that knows the semantics of an XML tag having a namespace
+    prefix.
+    """
+    def __new__(cls, nstag, *args):
+        return super(NamespacePrefixedTag, cls).__new__(cls, nstag)
+
+    def __init__(self, nstag):
+        self._pfx, self._local_part = nstag.split(':')
+        self._ns_uri = nsmap[self._pfx]
+
+    @property
+    def clark_name(self):
+        return '{%s}%s' % (self._ns_uri, self._local_part)
+
+    @property
+    def local_part(self):
+        """
+        Return the local part of the tag as a string. E.g. 'foobar' is
+        returned for tag 'f:foobar'.
+        """
+        return self._local_part
+
+    @property
+    def nsmap(self):
+        """
+        Return a dict having a single member, mapping the namespace prefix of
+        this tag to it's namespace name (e.g. {'f': 'http://foo/bar'}). This
+        is handy for passing to xpath calls and other uses.
+        """
+        return {self._pfx: self._ns_uri}
+
+    @property
+    def nspfx(self):
+        """
+        Return the string namespace prefix for the tag, e.g. 'f' is returned
+        for tag 'f:foobar'.
+        """
+        return self._pfx
+
+    @property
+    def nsuri(self):
+        """
+        Return the namespace URI for the tag, e.g. 'http://foo/bar' would be
+        returned for tag 'f:foobar' if the 'f' prefix maps to
+        'http://foo/bar' in nsmap.
+        """
+        return self._ns_uri
---
+++ tests/oxml/test_ns.py
@@ -0,0 +1,57 @@
+# encoding: utf-8
+
+"""
+Test suite for docx.oxml.ns
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import pytest
+
+from docx.oxml.ns import NamespacePrefixedTag
+
+
+class DescribeNamespacePrefixedTag(object):
+
+    def it_behaves_like_a_string_when_you_want_it_to(self, nsptag):
+        s = '- %s -' % nsptag
+        assert s == '- a:foobar -'
+
+    def it_knows_its_clark_name(self, nsptag, clark_name):
+        assert nsptag.clark_name == clark_name
+
+    def it_knows_its_local_part(self, nsptag, local_part):
+        assert nsptag.local_part == local_part
+
+    def it_can_compose_a_single_entry_nsmap_for_itself(
+            self, nsptag, namespace_uri_a):
+        expected_nsmap = {'a': namespace_uri_a}
+        assert nsptag.nsmap == expected_nsmap
+
+    def it_knows_its_namespace_prefix(self, nsptag):
+        assert nsptag.nspfx == 'a'
+
+    def it_knows_its_namespace_uri(self, nsptag, namespace_uri_a):
+        assert nsptag.nsuri == namespace_uri_a
+
+    # fixtures -------------------------------------------------------
+
+    @pytest.fixture
+    def clark_name(self, namespace_uri_a, local_part):
+        return '{%s}%s' % (namespace_uri_a, local_part)
+
+    @pytest.fixture
+    def local_part(self):
+        return 'foobar'
+
+    @pytest.fixture
+    def namespace_uri_a(self):
+        return 'http://schemas.openxmlformats.org/drawingml/2006/main'
+
+    @pytest.fixture
+    def nsptag(self, nsptag_str):
+        return NamespacePrefixedTag(nsptag_str)
+
+    @pytest.fixture
+    def nsptag_str(self, local_part):
+        return 'a:%s' % local_part
