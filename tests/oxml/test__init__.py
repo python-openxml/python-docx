eb3641156926b6ed37d4f94dfe9db3d400c8e3dd
Steve Canny <scanny@cisco.com>
2014-06-15 13:18:10 -0700

    oxml: add unit tests for parse_xml()

--- tests/oxml/test__init__.py
+++ tests/oxml/test__init__.py
@@ -10,7 +10,8 @@ import pytest
 
 from lxml import etree
 
-from docx.oxml import oxml_parser
+from docx.oxml import oxml_parser, parse_xml, register_custom_element_class
+from docx.oxml.shared import OxmlBaseElement
 
 
 class DescribeOxmlParser(object):
@@ -32,3 +33,47 @@ class DescribeOxmlParser(object):
         )
         stripped_xml_text = '<foø><bår>text</bår></foø>'
         return pretty_xml_text, stripped_xml_text
+
+
+class DescribeParseXml(object):
+
+    def it_accepts_bytes_and_assumes_utf8_encoding(self, xml_bytes):
+        parse_xml(xml_bytes)
+
+    def it_accepts_unicode_providing_there_is_no_encoding_declaration(self):
+        non_enc_decl = '<?xml version="1.0" standalone="yes"?>'
+        enc_decl = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
+        xml_body = '<foo><bar>føøbår</bar></foo>'
+        # unicode body by itself doesn't raise
+        parse_xml(xml_body)
+        # adding XML decl without encoding attr doesn't raise either
+        xml_text = '%s\n%s' % (non_enc_decl, xml_body)
+        parse_xml(xml_text)
+        # but adding encoding in the declaration raises ValueError
+        xml_text = '%s\n%s' % (enc_decl, xml_body)
+        with pytest.raises(ValueError):
+            parse_xml(xml_text)
+
+    def it_uses_registered_element_classes(self, xml_bytes):
+        register_custom_element_class('a:foo', CustElmCls)
+        element = parse_xml(xml_bytes)
+        assert isinstance(element, CustElmCls)
+
+    # fixture components ---------------------------------------------
+
+    @pytest.fixture
+    def xml_bytes(self):
+        return (
+            '<a:foo xmlns:a="http://schemas.openxmlformats.org/drawingml/200'
+            '6/main">\n'
+            '  <a:bar>foøbår</a:bar>\n'
+            '</a:foo>\n'
+        ).encode('utf-8')
+
+
+# ===========================================================================
+# static fixture
+# ===========================================================================
+
+class CustElmCls(OxmlBaseElement):
+    pass
