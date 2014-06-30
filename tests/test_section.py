574fea1f0eca9cac3e164c0c207a6a807e4868ac
Steve Canny <scanny@cisco.com>
2014-06-29 23:26:21 -0700

    test: refactor tests.test_section to use cxml

--- tests/test_section.py
+++ tests/test_section.py
@@ -12,7 +12,6 @@ from docx.enum.section import WD_ORIENT, WD_SECTION
 from docx.section import Section
 from docx.shared import Inches
 
-from .oxml.unitdata.section import a_pgMar, a_sectPr
 from .unitutil.cxml import element, xml
 
 
@@ -55,78 +54,58 @@ class DescribeSection(object):
         assert section._sectPr.xml == expected_xml
 
     def it_knows_its_page_margins(self, margins_get_fixture):
-        section, left, right, top, bottom, gutter, header, footer = (
-            margins_get_fixture
-        )
-        assert section.left_margin == left
-        assert section.right_margin == right
-        assert section.top_margin == top
-        assert section.bottom_margin == bottom
-        assert section.gutter == gutter
-        assert section.header_distance == header
-        assert section.footer_distance == footer
+        section, margin_prop_name, expected_value = margins_get_fixture
+        value = getattr(section, margin_prop_name)
+        assert value == expected_value
 
     def it_can_change_its_page_margins(self, margins_set_fixture):
         section, margin_prop_name, new_value, expected_xml = (
             margins_set_fixture
         )
-        print(section._sectPr.xml)
         setattr(section, margin_prop_name, new_value)
-        print(section._sectPr.xml)
         assert section._sectPr.xml == expected_xml
 
     # fixtures -------------------------------------------------------
 
     @pytest.fixture(params=[
-        (True,   720,  720,  720,  720,  720,  720,  720),
-        (True,  None,  360, None,  360, None,  360, None),
-        (False, None, None, None, None, None, None, None),
+        ('w:sectPr/w:pgMar{w:left=120}',   'left_margin',      76200),
+        ('w:sectPr/w:pgMar{w:right=240}',  'right_margin',    152400),
+        ('w:sectPr/w:pgMar{w:top=-360}',   'top_margin',     -228600),
+        ('w:sectPr/w:pgMar{w:bottom=480}', 'bottom_margin',   304800),
+        ('w:sectPr/w:pgMar{w:gutter=600}', 'gutter',          381000),
+        ('w:sectPr/w:pgMar{w:header=720}', 'header_distance', 457200),
+        ('w:sectPr/w:pgMar{w:footer=840}', 'footer_distance', 533400),
+        ('w:sectPr/w:pgMar',               'left_margin',       None),
+        ('w:sectPr',                       'top_margin',        None),
     ])
     def margins_get_fixture(self, request):
-        (has_pgMar_child, left, right, top, bottom, gutter, header,
-         footer) = request.param
-        pgMar_bldr = self.pgMar_bldr(**{
-            'has_pgMar': has_pgMar_child, 'left': left, 'right': right,
-            'top': top, 'bottom': bottom, 'gutter': gutter, 'header': header,
-            'footer': footer
-        })
-        sectPr = self.sectPr_bldr(pgMar_bldr).element
-        section = Section(sectPr)
-        expected_left = self.twips_to_emu(left)
-        expected_right = self.twips_to_emu(right)
-        expected_top = self.twips_to_emu(top)
-        expected_bottom = self.twips_to_emu(bottom)
-        expected_gutter = self.twips_to_emu(gutter)
-        expected_header = self.twips_to_emu(header)
-        expected_footer = self.twips_to_emu(footer)
-        return (
-            section, expected_left, expected_right, expected_top,
-            expected_bottom, expected_gutter, expected_header,
-            expected_footer
-        )
+        sectPr_cxml, margin_prop_name, expected_value = request.param
+        section = Section(element(sectPr_cxml))
+        return section, margin_prop_name, expected_value
 
     @pytest.fixture(params=[
-        ('left', 1440, 720), ('right', None, 1800), ('top', 2160, None),
-        ('bottom', 720, 2160), ('gutter', None, 360), ('header', 720, 630),
-        ('footer', None, 810)
+        ('w:sectPr', 'left_margin',     Inches(1),
+         'w:sectPr/w:pgMar{w:left=1440}'),
+        ('w:sectPr', 'right_margin',    Inches(0.5),
+         'w:sectPr/w:pgMar{w:right=720}'),
+        ('w:sectPr', 'top_margin',      Inches(-0.25),
+         'w:sectPr/w:pgMar{w:top=-360}'),
+        ('w:sectPr', 'bottom_margin',   Inches(0.75),
+         'w:sectPr/w:pgMar{w:bottom=1080}'),
+        ('w:sectPr', 'gutter',          Inches(0.25),
+         'w:sectPr/w:pgMar{w:gutter=360}'),
+        ('w:sectPr', 'header_distance', Inches(1.25),
+         'w:sectPr/w:pgMar{w:header=1800}'),
+        ('w:sectPr', 'footer_distance', Inches(1.35),
+         'w:sectPr/w:pgMar{w:footer=1944}'),
+        ('w:sectPr', 'left_margin', None, 'w:sectPr/w:pgMar'),
+        ('w:sectPr/w:pgMar{w:top=-360}', 'top_margin', Inches(0.6),
+         'w:sectPr/w:pgMar{w:top=864}'),
     ])
     def margins_set_fixture(self, request):
-        margin_side, initial_margin, new_margin = request.param
-        # section ----------------------
-        pgMar_bldr = self.pgMar_bldr(**{margin_side: initial_margin})
-        sectPr = self.sectPr_bldr(pgMar_bldr).element
-        section = Section(sectPr)
-        # property name ----------------
-        property_name = {
-            'left': 'left_margin', 'right': 'right_margin',
-            'top': 'top_margin', 'bottom': 'bottom_margin',
-            'gutter': 'gutter', 'header': 'header_distance',
-            'footer': 'footer_distance'
-        }[margin_side]
-        # expected_xml -----------------
-        pgMar_bldr = self.pgMar_bldr(**{margin_side: new_margin})
-        expected_xml = self.sectPr_bldr(pgMar_bldr).xml()
-        new_value = self.twips_to_emu(new_margin)
+        sectPr_cxml, property_name, new_value, expected_cxml = request.param
+        section = Section(element(sectPr_cxml))
+        expected_xml = xml(expected_cxml)
         return section, property_name, new_value, expected_xml
 
     @pytest.fixture(params=[
@@ -224,29 +203,3 @@ class DescribeSection(object):
         section = Section(element(initial_cxml))
         expected_xml = xml(expected_cxml)
         return section, new_start_type, expected_xml
-
-    # fixture components ---------------------------------------------
-
-    @staticmethod
-    def twips_to_emu(twips):
-        if twips is None:
-            return None
-        return twips * 635
-
-    def pgMar_bldr(self, **kwargs):
-        if kwargs.pop('has_pgMar', True) is False:
-            return None
-        pgMar_bldr = a_pgMar()
-        for key, value in kwargs.items():
-            if value is None:
-                continue
-            set_attr_method = getattr(pgMar_bldr, 'with_%s' % key)
-            set_attr_method(value)
-        return pgMar_bldr
-
-    def sectPr_bldr(self, *child_bldrs):
-        sectPr_bldr = a_sectPr().with_nsdecls()
-        for child_bldr in child_bldrs:
-            if child_bldr is not None:
-                sectPr_bldr.with_child(child_bldr)
-        return sectPr_bldr
