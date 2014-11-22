316b0039edfdcf9a135018fea6683559a16111cb
Steve Canny <scanny@cisco.com>
2014-11-21 21:09:31 -0800

    test: add snippet infrastructure

--- tests/unitutil/file.py
+++ tests/unitutil/file.py
@@ -27,6 +27,33 @@ def docx_path(name):
     return absjoin(test_file_dir, '%s.docx' % name)
 
 
+def snippet_seq(name, offset=0, count=1024):
+    """
+    Return a tuple containing the unicode text snippets read from the snippet
+    file having *name*. Snippets are delimited by a blank line. If specified,
+    *count* snippets starting at *offset* are returned.
+    """
+    path = os.path.join(test_file_dir, 'snippets', '%s.txt' % name)
+    with open(path, 'rb') as f:
+        text = f.read().decode('utf-8')
+    snippets = text.split('\n\n')
+    start, end = offset, offset+count
+    return tuple(snippets[start:end])
+
+
+def snippet_text(snippet_file_name):
+    """
+    Return the unicode text read from the test snippet file having
+    *snippet_file_name*.
+    """
+    snippet_file_path = os.path.join(
+        test_file_dir, 'snippets', '%s.txt' % snippet_file_name
+    )
+    with open(snippet_file_path, 'rb') as f:
+        snippet_bytes = f.read()
+    return snippet_bytes.decode('utf-8')
+
+
 def test_file(name):
     """
     Return the absolute path to test file having *name*.
