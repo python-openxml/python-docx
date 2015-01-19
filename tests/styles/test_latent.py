faf2dee5b4950f152105b82cfcde079c1a8eead3
Steve Canny <scanny@cisco.com>
2015-01-19 02:26:47 -0800

    style: add LatentStyles.__iter__()

--- tests/styles/test_latent.py
+++ tests/styles/test_latent.py
@@ -10,7 +10,7 @@ from __future__ import (
 
 import pytest
 
-from docx.styles.latent import LatentStyles
+from docx.styles.latent import _LatentStyle, LatentStyles
 
 from ..unitutil.cxml import element
 
@@ -21,7 +21,24 @@ class DescribeLatentStyles(object):
         latent_styles, expected_value = len_fixture
         assert len(latent_styles) == expected_value
 
+    def it_can_iterate_over_its_latent_styles(self, iter_fixture):
+        latent_styles, expected_count = iter_fixture
+        lst = [ls for ls in latent_styles]
+        assert len(lst) == expected_count
+        for latent_style in lst:
+            assert isinstance(latent_style, _LatentStyle)
+
     # fixture --------------------------------------------------------
+
+    @pytest.fixture(params=[
+        ('w:latentStyles',                                  0),
+        ('w:latentStyles/w:lsdException',                   1),
+        ('w:latentStyles/(w:lsdException,w:lsdException)',  2),
+    ])
+    def iter_fixture(self, request):
+        latentStyles_cxml, count = request.param
+        latent_styles = LatentStyles(element(latentStyles_cxml))
+        return latent_styles, count
 
     @pytest.fixture(params=[
         ('w:latentStyles',                                  0),
