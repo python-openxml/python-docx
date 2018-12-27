125a0dfb1dc81052742fc0bda81d3f6447dfd082
Benjamin Toornstra <benjamin.toornstra@vanoord.com>
2018-12-27 13:44:54 -0800

    bmk: add _PartBookmarkFinder._name_already_used()

--- docx/oxml/bookmark.py
+++ docx/oxml/bookmark.py
@@ -4,15 +4,18 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from docx.oxml.simpletypes import ST_String
+from docx.oxml.simpletypes import ST_DecimalNumber, ST_String
 from docx.oxml.xmlchemy import BaseOxmlElement, RequiredAttribute
 
 
 class CT_Bookmark(BaseOxmlElement):
     """`w:bookmarkStart` element"""
 
+    id = RequiredAttribute("w:id", ST_DecimalNumber)
     name = RequiredAttribute("w:name", ST_String)
 
 
 class CT_MarkupRange(BaseOxmlElement):
     """`w:bookmarkEnd` element"""
+
+    id = RequiredAttribute("w:id", ST_DecimalNumber)
