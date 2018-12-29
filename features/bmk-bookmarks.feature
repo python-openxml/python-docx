794af7de2fbc13da317273d890fca87cf48c7f2c
Benjamin Toornstra <benjamin.toornstra@vanoord.com>
2018-12-29 23:08:55 +0100

    acpt: Add scenario for named bookmark access

--- features/bmk-bookmarks.feature
+++ features/bmk-bookmarks.feature
@@ -9,3 +9,16 @@ Feature: Access a bookmark
      Then len(bookmarks) == 5
       And bookmarks[1] is a _Bookmark object
       And iterating bookmarks produces 5 _Bookmark objects
+
+  @wip
+  Scenario Outline: Bookmarks.get(bookmark_name)
+    Given a Bookmarks object of length 5 as bookmarks
+     Then bookmarks.get(<name>) returns bookmark named "<name>" with id <id>
+
+    Examples: Named Bookmarks
+        | name               | id |
+        | bookmark_body      | 2  |
+        | bookmark_endnote   | 1  |
+        | bookmark_footer    | 5  |
+        | bookmark_footnote  | 0  |
+        | bookmark_header    | 4  |
--- features/steps/bookmarks.py
+++ features/steps/bookmarks.py
@@ -2,9 +2,7 @@
 
 """Step implementations for bookmark-related features."""
 
-from __future__ import (
-    absolute_import, division, print_function, unicode_literals
-)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from behave import given, then
 
@@ -15,31 +13,40 @@ from helpers import test_docx
 
 # given ===================================================
 
-@given('a Bookmarks object of length 5 as bookmarks')
+
+@given("a Bookmarks object of length 5 as bookmarks")
 def given_a_Bookmarks_object_of_length_5_as_bookmarks(context):
-    document = Document(test_docx('bmk-bookmarks'))
+    document = Document(test_docx("bmk-bookmarks"))
     context.bookmarks = document.bookmarks
 
 
 # then =====================================================
 
-@then('bookmarks[{idx}] is a _Bookmark object')
+
+@then('bookmarks.get({name}) returns bookmark named "{name}" with id {id}')
+def then_bookmark_get_returns_bookmark_object(context, name, id):
+    bookmark = context.bookmarks.get(name)
+    assert bookmark.name == name
+    assert bookmark.id == int(id)
+
+
+@then("bookmarks[{idx}] is a _Bookmark object")
 def then_bookmarks_idx_is_a_Bookmark_object(context, idx):
     item = context.bookmarks[int(idx)]
-    expected = '_Bookmark'
+    expected = "_Bookmark"
     actual = item.__class__.__name__
-    assert actual == expected, 'bookmarks[%s] is a %s object' % (idx, actual)
+    assert actual == expected, "bookmarks[%s] is a %s object" % (idx, actual)
 
 
-@then('iterating bookmarks produces {n} _Bookmark objects')
+@then("iterating bookmarks produces {n} _Bookmark objects")
 def then_iterating_bookmarks_produces_n_Bookmark_objects(context, n):
     items = [item for item in context.bookmarks]
     assert len(items) == int(n)
-    assert all(item.__class__.__name__ == '_Bookmark' for item in items)
+    assert all(item.__class__.__name__ == "_Bookmark" for item in items)
 
 
-@then('len(bookmarks) == {count}')
+@then("len(bookmarks) == {count}")
 def then_len_bookmarks_eq_count(context, count):
     expected = int(count)
     actual = len(context.bookmarks)
-    assert actual == expected, 'len(bookmarks) == %s' % actual
+    assert actual == expected, "len(bookmarks) == %s" % actual
