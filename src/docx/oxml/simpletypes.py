513640de0281badc44b3b11d68ee216419ffcbc5
Long Nguyen <nguyen.hai.long-b@sun-asterisk.com>
2025-05-08 02:07:28 +0000

    ValueError int base 10

--- src/docx/oxml/simpletypes.py
+++ src/docx/oxml/simpletypes.py
@@ -346,7 +346,7 @@ class ST_TwipsMeasure(XsdUnsignedLong):
     def convert_from_xml(cls, str_value: str) -> Length:
         if "i" in str_value or "m" in str_value or "p" in str_value:
             return ST_UniversalMeasure.convert_from_xml(str_value)
-        return Twips(int(str_value))
+        return Twips(int(round(float(str_value))))
 
     @classmethod
     def convert_to_xml(cls, value: int | Length) -> str:
